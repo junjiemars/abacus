d75ce6ee04d25a82babf50865b7ecc3431b37467
南山 <junjiemars@gmail.com>
2015-05-04 23:59:20 +0800

    add present-value/+ times/+ fn

--- src/abacus/core.clj
+++ src/abacus/core.clj
@@ -5,17 +5,42 @@
   [a b]
   (Math/pow a b))
 
+(defn log
+  [a]
+  (Math/log10 a))
+
 (defn future-value
   "Returns the future value using simple interest. p is the principal, r is the
-  interest rate, and t is the time"
+  interest rate, and t is the times"
   [p r t]
   (* p (+ 1 (* r t))))
 
 (defn future-value+
-  "Returns the future value using add-on interest. p is the principal, r is the
-  interest rate, and t is the time"
+  "Returns the future value using compound interest. p is the principal, r is the
+  interest rate, and t is the times"
   [p r t]
   (* p (pow (+ 1 r) t)))
 
+(defn present-value
+  "Returns the present value using simple interest. f is the future value, r is 
+  the interest rate, and t is the times"
+  [f r t]
+  (/ f (+ 1 (* r t))))
 
+(defn present-value+
+  "Returns the present value using compound interest. f is the future value, r is
+  the interest rate, and t is the times"
+  [f r t]
+  (/ f (pow (+ 1 r) t)))
 
+(defn times
+  "Returns the times using simple interest. f is the future value, p is the 
+  principal, and r is the interest rate"
+  [f p r]
+  (/ (log (/ f p)) (log (+ 1 r))))
+
+(defn times+
+  "Returns the times using compound interest. f is the future value, p is the
+  principal, and r is the interest rate"
+  [f p r]
+  (/ (log (/ f p)) (log (+ 1 r))))
